98283871734d21fc319ca0338e03716c66756d61
ERwiin21MP <erwindev98@gmail.com>
2024-02-08 12:44:35 -0600

    Se ha creado el HomeViewModel.kt

---
+++ app/src/main/java/com/erwiin21mp/cinemovilplus/ui/view/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package com.erwiin21mp.cinemovilplus.ui.view.home
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() {
+
+}
